fa6789f15f03e45b4807664fbcad5d026a6601b2
Shane Ford <snford1@loyola.edu>
2024-01-18 12:25:22 -0500

    initials: SF which is shane ford in ASCII art form

---
+++ SFASCIIart.java
@@ -0,0 +1,17 @@
+// Programmers:  Shane Ford
+// Course:  CS 212, Dr. Cui
+// Due Date: 1/19/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None; there is no user input in this program
+// Output:  The initials of the programmers in ASCII art form
+
+public class SFASCIIart {
+    public static void main(String[] args){
+        System.out.println(" ____    ______ ");
+        System.out.println("/ ___|  |  ____|");
+        System.out.println("\\___ \\  |  |__");
+        System.out.println(" ___) | |  __|");
+        System.out.println("|____/  |_|");
+    }
+}
